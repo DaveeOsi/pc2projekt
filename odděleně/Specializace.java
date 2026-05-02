a50c3b5493517649e19b618dc07335d15a5235e8
sulcpetr <271009@vut.cz>
2026-05-02 12:34:56 +0200

    Update Specializace.java

--- odděleně/Specializace.java
+++ odděleně/Specializace.java
@@ -3,6 +3,6 @@ package projektpc2;
 import java.util.Map;
 
 public interface Specializace {
-    // Přidali jsme do závorky parametr, aby se shodoval s analytikem a specialistou
+   
     void provedDovednost(Map<Integer, Zamestnanec> vsichni);
 }
